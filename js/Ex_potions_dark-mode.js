ff3c1ddb070ca8078e7cf7e61c4392dc90fda4f8
qDocp <quentin.besneux@gmail.com>
2026-08-02 12:09:51 +0200

    2..2

--- js/Ex_potions_dark-mode.js
+++ js/Ex_potions_dark-mode.js
@@ -1,33 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     const body = document.body;
     const darkModeToggle = document.getElementById('darkModeToggle');
-    const lightIcon = darkModeToggle.querySelector('.icon-light');
-    const darkIcon = darkModeToggle.querySelector('.icon-dark');
 
     body.classList.add('no-transition');
 
-    function updateMode(isDarkMode) {
-        if (isDarkMode) {
-            body.classList.add('dark-mode');
-            lightIcon.style.transform = 'translateY(-50%) translateX(-30px)';
-            lightIcon.style.opacity = '0';
-            darkIcon.style.transform = 'translateY(-50%) translateX(0)';
-            darkIcon.style.opacity = '1';
-            localStorage.setItem('darkMode', 'enabled');
-        } else {
-            body.classList.remove('dark-mode');
-            lightIcon.style.transform = 'translateY(-50%) translateX(0)';
-            lightIcon.style.opacity = '1';
-            darkIcon.style.transform = 'translateY(-50%) translateX(30px)';
-            darkIcon.style.opacity = '0';
-            localStorage.setItem('darkMode', 'disabled');
-        }
-    }
-
     if (localStorage.getItem('darkMode') === 'enabled') {
-        updateMode(true);
+        body.classList.add('dark-mode');
     } else {
-        updateMode(false);
+        body.classList.remove('dark-mode');
     }
 
     setTimeout(() => {
@@ -36,8 +16,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (darkModeToggle) {
         darkModeToggle.addEventListener('click', () => {
-            const isDarkMode = body.classList.contains('dark-mode');
-            updateMode(!isDarkMode);
+            body.classList.toggle('dark-mode');
+            localStorage.setItem('darkMode', body.classList.contains('dark-mode') ? 'enabled' : 'disabled');
         });
     }
 
@@ -47,7 +27,6 @@ document.addEventListener('DOMContentLoaded', () => {
         header.addEventListener('click', () => {
             if(window.innerWidth <= 768) {
                 const currentBox = header.parentElement;
-                
                 const isActive = currentBox.classList.contains('active');
 
                 document.querySelectorAll('.info-box').forEach(box => {
